0687663ff33858956579fa81b9ee59b122107626
anas <ktmanas2020@gmail.com>
2024-06-28 15:30:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Button from 'react-bootstrap/Button'
+import  'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+            <Button>button</Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
