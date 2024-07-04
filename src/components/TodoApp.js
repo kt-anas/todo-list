3bed3b8e07768b269e824a1dc2ec8304242e8f79
anas <ktmanas2020@gmail.com>
2024-07-04 12:58:37 +0530

    update

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -6,33 +6,54 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 const TodoApp = () => {
   const [input, setInput] = useState("");
   const [items, setItems] = useState([]);
-
+  const [isEditing, setIsEditing] = useState(false);
+  const [currentIndex, setCurrentIndex] = useState(null);
+  
   const handleChange = (event) => {
     setInput(event.target.value);
   };
 
   const deleteItem = (index) => {
-    const allItems = items.filter((data,ind) => ind!== index); 
+    const allItems = items.filter((data, ind) => ind !== index);
     setItems(allItems);
   };
 
   const storeItem = (eve) => {
     eve.preventDefault();
-    setItems([...items, input]);
-    setInput("");
+    if (input.trim() !== " ") {
+      if (isEditing) {
+        const updatedItems = items.map((item, index) => 
+          index === currentIndex ? input.trim() : item
+        );
+        setItems(updatedItems);
+        setIsEditing(false);
+        setCurrentIndex(null);
+      } else {
+        setItems([...items, input.trim()]);
+      }
+      setInput("");
+    }
   };
 
+  const editItem = (index) => {
+    setInput(items[index]);
+    setIsEditing(true);
+    setCurrentIndex(index);
+  };
+    
   return (
     <div className='todo-container'>
       <form className='input-section' onSubmit={storeItem}>
         <h1>Todo App</h1>
-        <input type="text" value={input} onChange={handleChange} placeholder='Enter...' required />
-        <Button type="submit" class="btn btn-dark" value="Submit">ADD</Button>
+        <input type="text" value={input} onChange={handleChange} placeholder='Enter...' required/>
+        <Button type="submit" className="btn btn-dark">{isEditing ? "Update" : "ADD"}</Button>
       </form>
       <ul>
         {items.map((data, index) => (
           <li key={index}>
-            {data} <i className='bi bi-x-lg' onClick={() => deleteItem(index)}></i>
+            {data}
+            <i className='bi bi-pencil-fill' onClick={() => editItem(index)}></i>
+            <i className='bi bi-x-lg' onClick={() => deleteItem(index)}></i>
           </li>
         ))}
       </ul>
